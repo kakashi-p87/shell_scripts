8883deb3096ab92e2508334c68ec797b5fbc27d4
kakashi-p87 <acmilanp7@hotmail.com>
2017-12-10 21:22:29 -0600

    java configuration

--- ftp_config.sh
+++ ftp_config.sh
@@ -1,53 +1,62 @@
-echo "#####FTP configuration#####";
+echo "###########################################";
+echo "#		       FTP configuration              #";
+echo "###########################################";
 
-echo "====>Installing ftp...";
 apt-get -y install vsftpd;
-echo "====>done";
+echo "===========================================";
+echo "#		       vsftpd installed               #";
+echo "===========================================";
 
-echo "====>install & config xinetd";
 apt-get install xinetd;
+echo "===========================================";
+echo "#		       xinetd installed               #";
+echo "===========================================";
 
 vsftpd_file="/etc/xinetd.d/vsftpd";
 
-echo -e "service ftp" 					>> $vsftpd_file;
-echo -e "{"						>> $vsftpd_file;
-echo -e "	disable		= no" 			>> $vsftpd_file;
-echo -e "	socket_type	= stream"		>> $vsftpd_file;
-echo -e "	wait		= no"			>> $vsftpd_file;
-echo -e "	user		= root"			>> $vsftpd_file;
-echo -e "	server		= /usr/sbin/vsftpd"	>> $vsftpd_file;
-echo -e "	per_source	= 5"			>> $vsftpd_file;
-echo -e "	instances	= 200"			>> $vsftpd_file;
-echo -e "	no_access	= 10.1.1.10"		>> $vsftpd_file;
-echo -e "	banner_fail	= /etc/vsftpd.busy"	>> $vsftpd_file;
-echo -e "	log_on_success	+= PID HOST DURATION"	>> $vsftpd_file;
-echo -e "	log_on_failure	+= HOST"		>> $vsftpd_file;
-echo -e "}"						>> $vsftpd_file;
+echo -e "service ftp" 					                    >> $vsftpd_file;
+echo -e "{"					                              	>> $vsftpd_file;
+echo -e "	disable		= no" 	                    		>> $vsftpd_file;
+echo -e "	socket_type	= stream"		                  >> $vsftpd_file;
+echo -e "	wait		= no"			                        >> $vsftpd_file;
+echo -e "	user		= root"			                      >> $vsftpd_file;
+echo -e "	server		= /usr/sbin/vsftpd"	            >> $vsftpd_file;
+echo -e "	per_source	= 5"			                    >> $vsftpd_file;
+echo -e "	instances	= 200"			                    >> $vsftpd_file;
+echo -e "	no_access	= 10.1.1.10"		                >> $vsftpd_file;
+echo -e "	banner_fail	= /etc/vsftpd.busy"	          >> $vsftpd_file;
+echo -e "	log_on_success	+= PID HOST DURATION"	    >> $vsftpd_file;
+echo -e "	log_on_failure	+= HOST"		              >> $vsftpd_file;
+echo -e "}"						                              >> $vsftpd_file;
 
+echo "===========================================";
+echo "#		       xinetd configured              #";
+echo "===========================================";
 
-echo "====>done";
-
-echo "====>stopping";
+echo "==========================> stopping vsftpd";
 sed -i -e "s/\(listen=\).*/\1NO/" /etc/vsftpd.conf;
 sed -i -e "s/\(listen_ipv6=\).*/\1NO/" /etc/vsftpd.conf;
 echo "no_anon_password=YES" >> /etc/vsftpd.conf;
 /etc/init.d/vsftpd stop;
 
-echo "====>restart xinetd";
+echo "===========================> restart xinetd";
 #service xinetd restart;
 /etc/init.d/xinetd start;
 
-echo "====>done";
+echo "===========================> xinetd start";
 
 echo | netstat -ant | grep 21;
 
 apt-get install ftp;
+echo "===========================================";
+echo "#		       ftp installed                  #";
+echo "===========================================";
 
-echo "adding  admin user";
+echo "=======================> adding  admin user";
 echo "kakashi" > /etc/vsftpd.userlist;
 
 vsftpd_conf=/etc/vsftpd.conf;
 echo "userlist_file=/etc/vsftpd.userlist"		>> $vsftpd_conf;
-echo "userlist_enable=YES"				>> $vsftpd_conf;
-echo "userlist_deny=NO"					>> $vsftpd_conf;
-echo "local_root=/home/kakashi/ftp"			>> $vsftpd_conf;
+echo "userlist_enable=YES"				          >> $vsftpd_conf;
+echo "userlist_deny=NO"					            >> $vsftpd_conf;
+echo "local_root=/home/kakashi/ftp"			    >> $vsftpd_conf;
--- init_config.sh
+++ init_config.sh
@@ -1,54 +1,71 @@
 echo "###########################################";
-echo "#		Debian Configuration		#";
+echo "#		       Debian Configuration           #";
 echo "###########################################";
 
 echo "###########################################";
-echo "#		Sources Configuration		#";
+echo "#		       Sources Configuration       		#";
 echo "###########################################";
 
 sed -i '/cdrom/ s/#*//' /etc/apt/sources.list;
 sed -i '/cdrom/ s/^/#/' /etc/apt/sources.list;
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list;
+wget -q -O - http://dl.google.com/linux/linux_signing_key.pub | apt-key add -;
 
-echo "====>update..."; 
+echo "===========================================";
+echo "#		       Sources Configuration          #";
+echo "===========================================";
+
+echo "###########################################";
+echo "#		       apt-get update             		#";
+echo "###########################################";
 
 apt update;
 apt-get update;
-
-echo "====>done";
-
-echo "====>upgrade...";
 apt-get upgrade;
-echo "====>done";
 
-echo "====>build essential";
 apt-get -y install build-essential;
-echo "====>done";
+echo "===========================================";
+echo "#		       build-essential                #";
+echo "===========================================";
 
 apt-get -f net-tools;
+echo "===========================================";
+echo "#		       net-tools                #";
+echo "===========================================";
 
-echo "====>install git";
 apt-get -y install git;
-echo "====>done";
+echo "===========================================";
+echo "#		       git                             #";
+echo "===========================================";
 
-echo "====>install sudo";
 apt-get -y install sudo;
 echo "kakashi ALL=(ALL:ALL) ALL" >> /etc/sudoers;
+echo "===========================================";
+echo "#		       sudo                           #";
+echo "===========================================";
 
 
-echo "====>install ssh"
 apt-get -y install openssh-server;
-echo "====>done";
-
-echo "====>regenerate keys";
-
 #/etc/ssh/shhd_config
 #/etc/init.d/ssh restart
 rm /etc/ssh/ssh_host_*;
 dpkg-reconfigure openssh-server;
-echo "====>done";
+echo "===========================================";
+echo "#		       openssh-server                 #";
+echo "===========================================";
 
-wget -q -O - http://dl.google.com/linux/linux_signing_key.pub | apt-key add -;
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list;
-
-apt-get update;
 apt-get -y -f install google-chrome-stable;
+echo "===========================================";
+echo "#		       google-chrome-stable           #";
+echo "===========================================";
+
+
+apt-get -y -f install software-properties-common;
+add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main";
+apt-get update;
+apt-get -y install oracle-java8-installer;
+#update-alternatives --config java
+#update-alternatives --config command
+echo >> 'JAVA_HOME="/usr/lib/jvm/java-8-oracle"' >> /etc/enviroment;
+source /etc/enviroment;
+echo $JAVA_HOME;
