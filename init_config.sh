c801fe94f2285adfbaaeba197c02e64baeb38ba8
kakashi-p87 <acmilanp7@hotmail.com>
2017-12-11 11:02:56 -0600

    ssh config

--- init_config.sh
+++ init_config.sh
@@ -1,10 +1,10 @@
-echo "###########################################";
-echo "#		       Debian Configuration           #";
-echo "###########################################";
+echo "###################################################";
+echo "#		       Debian Configuration     	#";
+echo "###################################################";
 
-echo "###########################################";
-echo "#		       Sources Configuration       		#";
-echo "###########################################";
+echo "###################################################";
+echo "#		       Sources Configuration   		#";
+echo "###################################################";
 
 debian_ver=`lsb_release -c -s`;
 
@@ -17,22 +17,22 @@ apt-get -y -f install software-properties-common;
 #add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main";
 wget -q -O - http://dl.google.com/linux/linux_signing_key.pub | apt-key add -;
 
-echo "===========================================";
-echo "#		       Sources Configuration          #";
-echo "===========================================";
+echo "==================================================";
+echo "#		       Sources Configuration		#";
+echo "==================================================";
 
-echo "###########################################";
-echo "#		       apt-get update             		#";
-echo "###########################################";
+echo "###################################################";
+echo "#		       apt-get update           	#";
+echo "###################################################";
 
 apt update;
 apt-get update;
 apt-get upgrade;
 
 apt-get -y install build-essential;
-echo "===========================================";
-echo "#		       build-essential                #";
-echo "===========================================";
+echo "===================================================";
+echo "#		       build-essential			#";
+echo "===================================================";
 
 apt-get -f net-tools;
 echo "===========================================";
@@ -40,15 +40,15 @@ echo "#		       net-tools                #";
 echo "===========================================";
 
 apt-get -y install git;
-echo "===========================================";
-echo "#		       git                             #";
-echo "===========================================";
+echo "===================================================";
+echo "#		       git                              #";
+echo "===================================================";
 
 apt-get -y install sudo;
 echo "kakashi ALL=(ALL:ALL) ALL" >> /etc/sudoers;
-echo "===========================================";
-echo "#		       sudo                           #";
-echo "===========================================";
+echo "===================================================";
+echo "#		       sudo				#";
+echo "===================================================";
 
 
 apt-get -y install openssh-server;
@@ -56,6 +56,7 @@ apt-get -y install openssh-server;
 #/etc/init.d/ssh restart
 rm /etc/ssh/ssh_host_*;
 dpkg-reconfigure openssh-server;
+sed -i '/Port/9 sed -i '/Port\ 22/ s/#*//' /etc/ssh/sshd_config;
 echo "===========================================";
 echo "#		       openssh-server                 #";
 echo "===========================================";
@@ -70,9 +71,9 @@ echo "===========================================";
 
 
 ####
-apt-get -y install oracle-java8-installer;
+#apt-get -y install oracle-java8-installer;
 #update-alternatives --config java
 #update-alternatives --config command
-echo >> 'JAVA_HOME="/usr/lib/jvm/java-8-oracle"' >> /etc/enviroment;
-source /etc/enviroment;
-echo $JAVA_HOME;
+#echo >> 'JAVA_HOME="/usr/lib/jvm/java-8-oracle"' >> /etc/enviroment;
+#source /etc/enviroment;
+#echo $JAVA_HOME;
