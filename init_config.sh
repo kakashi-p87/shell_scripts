8b07b730ed75f5ec1f63cb666c2b393f85a06448
kakashi-p87 <acmilanp7@hotmail.com>
2017-12-10 20:12:29 -0600

    adding chrome and net tools

--- init_config.sh
+++ init_config.sh
@@ -1,6 +1,19 @@
-echo "#####Debian configuration#####";
+echo "###########################################";
+echo "#		Debian Configuration		#";
+echo "###########################################";
+
+echo "###########################################";
+echo "#		Sources Configuration		#";
+echo "###########################################";
+
+sed -i '/cdrom/ s/#*//' /etc/apt/sources.list;
+sed -i '/cdrom/ s/^/#/' /etc/apt/sources.list;
+
 echo "====>update..."; 
+
+apt update;
 apt-get update;
+
 echo "====>done";
 
 echo "====>upgrade...";
@@ -10,6 +23,8 @@ echo "====>done";
 echo "====>build essential";
 apt-get -y install build-essential;
 echo "====>done";
+
+apt-get -f net-tools;
 
 echo "====>install git";
 apt-get -y install git;
@@ -25,7 +40,15 @@ apt-get -y install openssh-server;
 echo "====>done";
 
 echo "====>regenerate keys";
+
+#/etc/ssh/shhd_config
+#/etc/init.d/ssh restart
 rm /etc/ssh/ssh_host_*;
 dpkg-reconfigure openssh-server;
 echo "====>done";
 
+wget -q -O - http://dl.google.com/linux/linux_signing_key.pub | apt-key add -;
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list;
+
+apt-get update;
+apt-get -y -f install google-chrome-stable;
