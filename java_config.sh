2343afb8b1220a86dbb043f0e71e3fca613bce83
kakashi-p87 <acmilanp7@hotmail.com>
2017-12-12 14:54:17 -0600

    validations

--- java_config.sh
+++ java_config.sh
@@ -1,11 +1,26 @@
-wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/9.0.1+11/jdk-9.0.1_linux-x64_bin.tar.gz;
-directory_java="/opt/java/";
-if [ -d $directory_java ]; then
-  sudo mkdir /opt/java/;
+
+java_directory="/opt/java/";
+jdk_directory="jdk-9.0.1/";
+java_file="jdk-9.0.1_linux-x64_bin.tar.gz";
+
+wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/9.0.1+11/${java_file};
+
+if [ ! -d ${java_directory} ]; then
+  sudo mkdir $java_directory;
 fi
-sudo mv jdk-9.0.1_linux-x64_bin.tar.gz /opt/java/;
-sudo tar -zxf /opt/java/jdk-9.0.1_linux-x64_bin.tar.gz /opt/java/;
-sudo rm /opt/java/jdk-9.0.1_linux-x64_bin.tar.gz;
+
+if [ ! -f ${java_directory}${java_file} ]; then
+  sudo mv ${java_file} ${java_directory};
+fi
+
+if [ ! -d ${java_directory}${jdk_directory} ]; then
+  sudo tar -zxf ${java_directory}${java_file} ${java_directory};
+  sudo rm ${java_directory}${java_file};
+fi
+
+if [ ! -f ${java_file} ]; then
+  sudo rm ${java_file};
+fi
 
 sudo update-alternatives --install /usr/bin/java java /opt/java/jdk-9.0.1/bin/java 100;
 sudo update-alternatives --install /usr/bin/java javac /opt/java/jdk-9.0.1/bin/javac 100;
