c3bf8d192d78eb8f40a05013b07c807eba60c6fe
kakashi-p87 <acmilanp7@hotmail.com>
2017-12-13 00:50:01 -0600

    mysql config file

---
+++ mysql_config.sh
@@ -0,0 +1,6 @@
+echo "###################################################";
+echo "#		       MySQL Configuration     	#";
+echo "###################################################";
+
+repo_mysql="mysql-apt-config_0.8.9-1_all.deb";
+wget https://repo.mysql.com//${repo_mysql};
