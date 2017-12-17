d1240a15119c6b38dd20aa619e0b889f435e9f31
kakashi-p87 <acmilanp7@hotmail.com>
2017-12-17 02:16:19 -0600

    issues

--- java_config.sh
+++ java_config.sh
@@ -5,20 +5,24 @@ java_directory="/opt/java";
 jdk_directory="/jdk-9.0.1";
 java_file="jdk-9.0.1_linux-x64_bin.tar.gz";
 
-if [ ! -d "$java_file" ]; then
-wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/9.0.1+11/$java_file;
+if [ ! -f "$java_file" -a -f "$java_directory/$java_file" ]; then
+  wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/9.0.1+11/$java_file;
+  echo "$java_file downloaded";
 fi
 
 if [ ! -d "$java_directory" ]; then
   sudo mkdir $java_directory;
+  echo "$java_directory created";
 fi
 
 if [ ! -f "$java_directory/$java_file" ]; then
   sudo mv $java_file $java_directory;
+  echo "$java_file moved";
 fi
 
 if [ ! -d "$java_directory$jdk_directory" ]; then
   sudo tar -zxf "$java_directory/$java_file" $java_directory;
+  echo "$java_file unzip";
 fi
 
 #if [ -f "$java_file" ]; then
@@ -26,7 +30,7 @@ fi
 #fi
 
 if [ -f "$java_directory/$java_file" ]; then
-  sudo rm $java_directory/$java_file;
+  sudo rm "$java_directory/$java_file";
 fi
 
 sudo update-alternatives --install /usr/bin/java java $java_directory$jdk_directory/bin/java 100;
