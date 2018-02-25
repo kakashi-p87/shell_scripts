5010d91ec79a6ddc80307f1722c5050be9b54bfb
kakashi-p87 <acmilanp7@hotmail.com>
2018-02-25 02:05:00 -0600

    issues

--- java_config.sh
+++ java_config.sh
@@ -1,24 +1,39 @@
 echo "###################################################";
 echo "#		               Java Configuration     	      #";
 echo "###################################################";
-java_directory="/opt/java";
-jdk_directory="/jdk-9.0.1";
-java_file="jdk-9.0.1_linux-x64_bin.tar.gz";
+java_dir="/opt/java";
+#jdk_dir="/jdk-9.0.1";
+#java_file="jdk-9.0.1_linux-x64_bin.tar.gz";
+jdk_dir="/jdk1.8.0_161";
+java_file="jdk-8u161-linux-x64.tar.gz";
+
+url_v='http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-linux-x64.tar.gz';
 
 if [ ! -f "$java_file" ]; then
-  wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/9.0.1+11/${java_file};
-  echo "$java_file downloaded";
+  wget --header "Cookie: oraclelicense=accept-securebackup-cookie" $url_v; 
+  #http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-linux-x64.tar.gz; 
+  #http://download.oracle.com/otn-pub/java/jdk/9.0.1+11/${java_file};
 fi
 
-if [ ! -d "$java_directory" ]; then
-  sudo mkdir $java_directory;
-  echo "$java_directory created";
+if [ ! -f "$java_file" ]; then
+  echo "Error: ${1:-"Unknown Error"}" 1>&2
+  exit 1;
 fi
 
-if [ ! -d "$java_directory$jdk_directory" ]; then
-  sudo tar x -C $java_directory -f $java_file;
+echo "$java_file downloaded";
+
+if [ ! -d "$java_dir" ]; then
+  sudo mkdir $java_dir;
+  echo "$java_dir created";
+fi
+
+if [ -d "$java_dir" ]; then
+  sudo tar x -C $java_dir -f $java_file;
+  rm $java_file;  
   echo "$java_file unzip";
-fi
+
+  sudo chown -R root:root $java_dir;
+  #sudo chmod -R g+w $java_directory;  
 
 #if [ -f "$java_file" ]; then
 #  sudo rm $java_file;
@@ -28,36 +43,38 @@ fi
 #  sudo rm "$java_directory/$java_file";
 #fi
 
-sudo update-alternatives --install /usr/bin/java java $java_directory$jdk_directory/bin/java 100;
-sudo update-alternatives --install /usr/bin/javac javac $java_directory$jdk_directory/bin/javac 100;
+  sudo update-alternatives --install /usr/bin/java java $java_dir$jdk_dir/bin/java 100;
+  sudo update-alternatives --install /usr/bin/javac javac $java_dir$jdk_dir/bin/javac 100;
 
-echo "###################################################";
-echo "#		             Java Configuration               #";
-echo "###################################################";
-sudo update-alternatives --config java;
-echo "###################################################";
-echo "#		             javac Configuration   	          #";
-echo "###################################################";
-sudo update-alternatives --config javac;
+  echo "###################################################";
+  echo "#		             Java Configuration               #";
+  echo "###################################################";
+  sudo update-alternatives --config java;
+  echo "###################################################";
+  echo "#		             javac Configuration   	          #";
+  echo "###################################################";
+  sudo update-alternatives --config javac;
 
-echo "###################################################";
-echo "#		                 Java Version     	          #";
-echo "###################################################";
-echo `java -version`;
-echo "###################################################";
-echo "#		            Java Compiler Version     	      #";
-echo "###################################################";
-echo `javac -version`;
+  echo "###################################################";
+  echo "#		                 Java Version     	          #";
+  echo "###################################################";
+  echo `java -version`;
+  echo "###################################################";
+  echo "#		            Java Compiler Version     	      #";
+  echo "###################################################";
+  echo `javac -version`;
 
-echo "###################################################";
-echo "#		                  JAVA_HOME     	            #";
-echo "###################################################";
-#sudo chown -R root:${USER} /etc/enviroment;
-#sudo chmod -R g+w /etc/enviroment;
-echo "JAVA_HOME='$java_directory$jdk_directory'" | sudo tee -a /etc/enviroment;
-echo "source /etc/enviroment" | sudo tee -a ~/.bashrc;
-sudo -s source /etc/enviroment;
-echo "===================================================";
-echo "#		                  JAVA_HOME     	            #";
-echo "#                     $JAVA_HOME                  #";
-echo "===================================================";
+  echo "###################################################";
+  echo "#		                  JAVA_HOME     	            #";
+  echo "###################################################";
+  #sudo chown -R root:${USER} /etc/enviroment;
+  #sudo chmod -R g+w /etc/enviroment;
+  echo "JAVA_HOME='$java_dir$jdk_dir'" | sudo tee -a /etc/enviroment;
+  echo "source /etc/enviroment" | sudo tee -a ~/.bashrc;
+  sudo -s source /etc/enviroment;
+  echo "===================================================";
+  echo "#		                  JAVA_HOME     	            #";
+  echo "#                     $JAVA_HOME                  #";
+  echo "===================================================";
+
+fi
--- mysql_config.sh
+++ mysql_config.sh
@@ -3,15 +3,18 @@ echo "#		       MySQL Configuration     	#";
 echo "###################################################";
 
 echo "please provide your mysql password";
-read password_mysql;
+read mysql_pass;
 
 repo_mysql="mysql-apt-config_0.8.9-1_all.deb";
 wget https://repo.mysql.com//${repo_mysql};
+
+sudo apt-get --purge remove mysql-server;
 
 sudo dpkg -i $repo_mysql;
 sudo apt-get update;
 
 sudo apt-get install mysql-server;
+rm $repo_mysql;
 
 #sudo service mysql status;
 
@@ -21,7 +24,8 @@ sudo service mysql start;
 
 #sudo apt-get -y install mysql-workbench-community;
 
-sudo mysql -u root -B -e "USE mysql;";
+#sudo mysql -u root --password=$mysql_pass -B -e "USE mysql;";
+sudo mysql -u root -B -e "USE mysql;"; 
 echo "=======================================================";
 sudo mysql -u root -B -e "SELECT User, Host, plugin FROM mysql.user;";
 echo "=======================================================";
