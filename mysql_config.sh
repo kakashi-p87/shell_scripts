67b389d24b2d1a739be709ce104e78e37e210c5b
kakashi-p87 <acmilanp7@hotmail.com>
2018-02-25 02:05:00 -0600

    issues

--- mysql_config.sh
+++ mysql_config.sh
@@ -5,10 +5,13 @@ echo "###################################################";
 repo_mysql="mysql-apt-config_0.8.9-1_all.deb";
 wget https://repo.mysql.com//${repo_mysql};
 
+sudo apt-get --purge remove mysql-server;
+
 sudo dpkg -i $repo_mysql;
 sudo apt-get update;
 
 sudo apt-get install mysql-server;
+rm $repo_mysql;
 
 #sudo service mysql status;
 
@@ -17,3 +20,4 @@ sudo service mysql stop;
 sudo service mysql start;
 
 #sudo apt-get -y install mysql-workbench-community;
+
