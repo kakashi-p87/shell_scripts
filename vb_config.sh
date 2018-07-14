6da83d3dd7515c15bda0a4131cf57881400c2d9c
kakashi-p87 <acmilanp7@hotmail.com>
2018-07-13 19:16:20 -0500

    virtual box config

--- vb_config.sh
+++ vb_config.sh
@@ -1,5 +1,6 @@
-
+#before this mount the virtualbox-guest iso in cdrom0
 sudo apt-get update;
 sudo apt-get upgrade;
 sudo apt-get install -y -f dkms;
-sudo apt-get install -y -f virtualbox-guest-dkms;
+#sudo apt-get install -y -f virtualbox-guest-dkms;
+sudo sh /media/cdrom0/autorun.sh
