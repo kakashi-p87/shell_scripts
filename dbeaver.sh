5617a1cf4b3a0d81d88f88d994c8ce102765ef01
kakashi-p87 <acmilanp7@hotmail.com>
2018-07-13 20:31:46 -0500

    more features

--- dbeaver.sh
+++ dbeaver.sh
@@ -1,6 +1,14 @@
 file_dbeaver="dbeaver-ce_5.0.0_amd64.deb";
+version="5.0.0";
+prefix="dbeaver-ce_";
+arch="_amd64.deb"
 
-wget https://dbeaver.jkiss.org/files/5.0.0/$file_dbeaver;
+echo "Please provide a valid version";
+read version;
+
+file_dbeaver=$prefix$version$arch;
+
+wget https://dbeaver.jkiss.org/files/$version/$file_dbeaver;
 
 sudo dpkg -i $file_dbeaver;
 
