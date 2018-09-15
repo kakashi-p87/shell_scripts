c4f6ee47121fc7daed4d0e4475b0141302ce8736
kakashi-p87 <acmilanp7@hotmail.com>
2018-09-15 18:35:27 -0500

    git configuration

--- init_config.sh
+++ init_config.sh
@@ -67,39 +67,3 @@ apt-get -y -f install google-chrome-stable;
 echo "===================================================";
 echo "#     		       google-chrome-stable             #";
 echo "===================================================";
-
-
-echo "###################################################";
-echo "#		             Git configuration        	      #";
-echo "###################################################";
-
-apt-get -y install git;
-
-echo "please provide your e-mail";
-read email_git;
-echo "please provide your git\'s user name";
-read user_git;
-
-git config --global user.email $email_git;
-git config --global user.name  $user_git;
-
-repo_dir="/home/$user_name/Repository";
-
-mkdir $repo_dir;
-chown -R $user_name:$user_name $repo_dir;
-
-mkdir "$repo_dir/shell_scripts";
-
-
-git clone https://github.com/kakashi-p87/shell_scripts.git "$repo_dir/shell_scripts";
-
-
-
-chown -R $user_name:$user_name $repo_dir/*
-
-
-echo `git config --list`;
-
-echo "===================================================";
-echo "#		               git configured                 #";
-echo "===================================================";
