13bb719f45e284cd1c5c593239909350ca7e886e
kakashi-p87 <acmilanp7@hotmail.com>
2018-09-16 01:08:49 -0500

    installing kenel headers

--- vb_config.sh
+++ vb_config.sh
@@ -3,4 +3,6 @@ sudo apt-get update;
 sudo apt-get upgrade;
 sudo apt-get install -y -f dkms;
 #sudo apt-get install -y -f virtualbox-guest-dkms;
+sudo apt-get install -f linux-headers-amd64;
+sudo apt-get install -f linux-headers-4.9.0-6-amd64;
 sudo sh /media/cdrom0/autorun.sh
