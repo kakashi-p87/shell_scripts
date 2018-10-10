4a49681d7c1b52db20b21a12e550d0fae7b8569d
kakashi-p87 <acmilanp7@hotmail.com>
2018-10-09 20:08:57 -0500

    adding email server and sonarqube images

--- docker_images.sh
+++ docker_images.sh
@@ -11,3 +11,26 @@ docker run -d -p 27017:27017 --name mongo-db mongo;
 
 #init bash mongo
 #docker exec -it mongo-db bash
+
+
+docker pull sonarque;
+
+docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
+
+
+docker pull analogic/poste.io;
+
+docker run -d \
+    -p 25:25 \
+    -p 80:80 \
+    -p 110:110 \
+    -p 143:143 \
+    -p 443:443 \
+    -p 587:587 \
+    -p 993:993 \
+    -p 995:995 \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /your-data-dir/data:/data \
+    --name "mailserver" \
+    -h "mail.example.com" \
+    -t analogic/poste.io;
