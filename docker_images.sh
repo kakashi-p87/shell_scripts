e5c5ebe2d6aff86cb8a40a9e57e1275d46260f35
kakashi-p87 <acmilanp7@hotmail.com>
2019-01-24 22:41:49 -0600

    adding jenkins volume and commands

--- docker_images.sh
+++ docker_images.sh
@@ -1,9 +1,21 @@
+docker volume create jenkinsVol;
+
 #docker pull jenkins/jenkins:lts;
 docker pull jenkins/jenkins;
 
-docker run -d -p 9090:8080 -p 50000:50000 jenkins/jenkins;
+#docker run -d -p 9090:8080 -p 50000:50000 jenkins/jenkins;
 #docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts;
+docker run -d \
+	-p 9090:8080  \
+	-p 50000:50000  \
+	--name jenkins \
+	--mount source=jenkinsVol,target=/app \
+	jenkins/jenkins;
 
+#docker exec -it jenkins bash
+#cat /var/jenkins_home/secrets/initialAdminPassword
+#docker start jenkins
+#docker stop jenkins
 
 docker pull mongo;
 #docker run --name mongo-db mongo;
