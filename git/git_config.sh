c90f2f2d5403735db4d979408f68e27517cd2231
kakashi-p87 <acmilanp7@hotmail.com>
2019-06-28 22:42:48 -0500

    adding repositories

--- git/git_config.sh
+++ git/git_config.sh
@@ -1,5 +1,5 @@
 echo "###################################################";
-echo "#		             Git configuration        	      #";
+echo "################ Git configuration ################";
 echo "###################################################";
 
 sudo apt-get -y  -f install git;
@@ -17,18 +17,9 @@ repo_dir="/home/$USER/Repositories";
 mkdir $repo_dir;
 chown -R $USER:$USER $repo_dir;
 
-mkdir "$repo_dir/shell_scripts";
+git config --global alias.lg "log --pretty='%Cred%h%Creset |%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset %C(cyan)[%an]%Creset' --graph"
 
-
-git clone https://github.com/kakashi-p87/shell_scripts.git "$repo_dir/shell_scripts";
-
-
-
-chown -R $USER:$USER $repo_dir/*
-
-
+echo "###################################################";
+echo "################## Git configured #################";
 echo `git config --list`;
-
-echo "===================================================";
-echo "#		               git configured                 #";
-echo "===================================================";
+echo "###################################################";
