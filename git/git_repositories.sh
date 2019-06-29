7cf7b8788d85b41cafb2465fddd1e3017cb11d16
kakashi-p87 <acmilanp7@hotmail.com>
2019-06-28 22:42:48 -0500

    adding repositories

---
+++ git/git_repositories.sh
@@ -0,0 +1,11 @@
+repo_dir="/home/$USER/Repositories";
+
+git clone https://github.com/Nozomi-Soft/env-configuration.git "$repo_dir/env-configuration";
+
+git clone https://github.com/Nozomi-Soft/common-lib.git "$repo_dir/common-lib";
+
+git clone https://github.com/Nozomi-Soft/rest-user.git "$repo_dir/rest-user";
+
+git clone https://github.com/Nozomi-Soft/nozomi-web.git "$repo_dir/nozomi-web";
+
+chown -R $USER:$USER $repo_dir/*;
