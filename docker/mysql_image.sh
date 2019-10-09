409fec8709e96efb900bf7119f60aece80365a75
kakashi-p87 <acmilanp7@hotmail.com>
2019-10-09 16:22:43 -0500

    mysql port changed

--- docker/mysql_image.sh
+++ docker/mysql_image.sh
@@ -1,10 +1,23 @@
 docker pull mysql/mysql-server:latest;
 
-docker run --name=mysql01 -d mysql/mysql-server:latest;
+#docker run --name=mysql-dev -d mysql/mysql-server:latest -p 3306:3306;
+docker run -d -p 9501:3306 --name=mysql-dev --env="MYSQL_ROOT_PASSWORD=123456" mysql/mysql-server:latest;
 
-docker logs mysql01;
+docker logs mysql-dev;
 
-#docker exec -it mysql01 mysql -uroot -p
+#docker exec -it mysql-dev mysql -u root -p
 
 #ALTER USER 'root'@'localhost' IDENTIFIED BY 'newpassword';
+
+#CREATE USER 'nozomi'@'%' IDENTIFIED BY 'pass';
+#GRANT ALL PRIVILEGES ON *.* TO 'nozomi'@'%' WITH GRANT OPTION;
+#FLUSH PRIVILEGES;
+
+#CREATE DATABASE nozomidb;
+#USE nozomidb;
+
+#CREATE DATABASE sonar;
+#CREATE USER 'sonar'@'%' IDENTIFIED BY 'sonar';
+#GRANT ALL PRIVILEGES ON *.* TO 'sonar'@'%' WITH GRANT OPTION;
+#FLUSH PRIVILEGES;
 
