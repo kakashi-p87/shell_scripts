1ea07861844c1d083d0c9e5a21a8d197f7634b3a
kakashi-p87 <acmilanp7@hotmail.com>
2021-07-03 18:35:57 -0500

    styles repo added

--- git/git_repositories.sh
+++ git/git_repositories.sh
@@ -2,6 +2,8 @@ repo_dir="/home/$USER/Repositories";
 
 git clone https://github.com/Nozomi-Soft/env-configuration.git "$repo_dir/env-configuration";
 
+git clone https://github.com/google/styleguide.git "$repo_dir/styleguide";
+
 git clone https://github.com/Nozomi-Soft/common-lib.git "$repo_dir/common-lib";
 
 git clone https://github.com/Nozomi-Soft/rest-user.git "$repo_dir/rest-user";
